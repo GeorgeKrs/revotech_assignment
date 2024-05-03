0bd3698db3e5159564a631869dacfeabf3510827
George <gkoursoumis97@gmail.com>
2024-05-03 15:40:54 +0300

    auth middleware

--- server/src/Http/Middlewares/AuthCheckMiddleware.js
+++ server/src/Http/Middlewares/AuthCheckMiddleware.js
@@ -1,4 +1,10 @@
+import ApiResponseDto from "../ApiHelpers/ApiResponseDto.js";
+
 const AuthCheckMiddleware = (req, res, next) => {
+  if (!req.params.sessionToken) {
+    return res.status(401).json(ApiResponseDto.unauthorized());
+  }
+
   return next();
 };
 
